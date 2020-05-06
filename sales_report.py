196750fc1315e4a01e5b13ea7731abb15ea0407c
Chantel Yip <chantelyip422@gmail.com>
2020-05-05 23:14:57 -0700

    added comments in each line to describe what the program is doing

--- sales_report.py
+++ sales_report.py
@@ -1,25 +1,28 @@
 """Generate sales report showing total melons each salesperson sold."""
 
+ 
+salespeople = [] #empty list for salespeople
+melons_sold = [] #empty list for melons sold
 
-salespeople = []
-melons_sold = []
+f = open('sales-report.txt') #open file
+for line in f: #looping over each line in file
+    line = line.rstrip() #stripping off trailing white spaces on the right of each line
+    entries = line.split('|') #split words where 'pipe' is to distinguish individual entries, creating a list of data
 
-f = open('sales-report.txt')
-for line in f:
-    line = line.rstrip()
-    entries = line.split('|')
+    salesperson = entries[0] #each salesperson's name is at index 0
+    melons = int(entries[2]) #melons sold is at index 3 
 
-    salesperson = entries[0]
-    melons = int(entries[2])
-
-    if salesperson in salespeople:
-        position = salespeople.index(salesperson)
-
+    if salesperson in salespeople: #if salesperson is already in `salespeople`, increment the no. of
+    #melons they've sold.
+    # Otherwise, add the salesperson's name to `salespeople` and
+    # `melons` to `melons_sold`
+        position = salespeople.index(salesperson)  #Find the position where `salesperson` is stored in `salespeople`
+        # Use that position to index into `melons_sold`
         melons_sold[position] += melons
     else:
         salespeople.append(salesperson)
         melons_sold.append(melons)
 
-
+# Loop over indices of `salespeople` and use it to index into `salespeople` and # `melons_sold`.
 for i in range(len(salespeople)):
     print(f'{salespeople[i]} sold {melons_sold[i]} melons')
